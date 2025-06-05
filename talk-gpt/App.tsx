05d01f154b49cfe723adb422a94df1bbd04e0c06
S Ray <selmen.rayen.92@gmail.com>
2025-06-05 23:37:34 +0100

    prepare ui

--- talk-gpt/App.tsx
+++ talk-gpt/App.tsx
@@ -1,10 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{ fontSize: 32, fontWeight: "bold", marginBottom: 30 }}>
+        Talk GPT 123
+      </Text>
+      <Text style={{ textAlign: "center" }}>
+        Press and hold this button to record your voice.Release the button to
+        send the recording, and you'll hear a response.
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +19,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
